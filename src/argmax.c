0d98ba93845ddd9c69866f297b2e45586698de18
root <longpeng96@gmail.com>
2022-04-20 19:38:20 +0800

    draft, v2

--- src/argmax.c
+++ src/argmax.c
@@ -1,16 +1,28 @@
-  int max=arr[0];
-  for(int i=0;i<len;i++){
-     if(arr[i]>max){
-            max=arr[i]
-       }
-   }
+#include <stdio.h>
+int argmax(int arr[],int len);
 
-   for(int i=0;i<len;i++){
-        if(arr[i]==max){
-            return i;
-       }
-   }
+int main(){
+    int arr[]={3,-42,432,7,-5,6,5,-144,2};
+    int res=argmax(arr,9);
+    printf("%d",res);
+}
 
+int argmax(int arr[],int len){
+    int max=arr[0];
+    int index=0;
+    for(int i=0;i<len;i++){
+        if(arr[i]>max){
+            max=arr[i];
+            index=i;
+        }
+    }
+    return index;
+
+}
+  
+/*
 s0->arr s1->len
-s2->i   t0->the copy of original s0
-s3->max s4->arr[i]
+s2->i   s3->max 
+s4->arr[i]
+3 -42 432 7 -5 6 5 -114 2 
+*/
