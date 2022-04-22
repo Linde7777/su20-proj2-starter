6cd90f70ceaf1fb2674d347dc3d0653c1d590134
root <longpeng96@gmail.com>
2022-04-22 16:38:47 +0800

    completed, v1

--- src/matmul.s
+++ src/matmul.s
@@ -34,38 +34,83 @@ matmul:
 
 
     # Prologue
-
+    addi sp,sp,-52
+    sw ra,0(sp)
+    sw s0,4(sp)
+    sw s1,8(sp)
+    sw s2,12(sp)
+    sw s3,16(sp)
+    sw s4,20(sp)
+    sw s5,24(sp)
+    sw s6,28(sp)
+    sw s7,32(sp)
+    sw s8,36(sp)
+    sw s9,40(sp)
+    sw s10,44(sp)
+    sw s11,48(sp)
+    
+    # TODO 
+    # mv s0, a0
+    
+    add s7,s1,x0
+    add s8,s1,x0
+    add s9,s6,x0
+    add s10,x0,x0
+    add s11,x0,x0
 
 outer_loop_start:
+    bge s10,s7,outer_loop_end
 
+    # la ra,outer_loop_start
+    jal ra,inner_loop_start
+    addi s10,s10,1
+    
+    jal x0,outer_loop_start
 
 
 
 inner_loop_start:
+    bge s11,s8,inner_loop_end
+    
+    mv a0,s0
+    mv a1,s3
+    mv a2,s2
+    li a3,1
+    mv a4,s5
+    jal ra,dot
 
+    sw a0,0(s9)
+    addi s9,s9,1
+    addi s11,s11,1
 
-
-
-
-
-
-
-
-
+    jal x0,inner_loop_start
 
 
 inner_loop_end:
-
-
+    ret
 
 
 outer_loop_end:
-
-
+    
     # Epilogue
+    lw ra,0(sp)
+    lw s0,4(sp)
+    lw s1,8(sp)
+    lw s2,12(sp)
+    lw s3,16(sp)
+    lw s4,20(sp)
+    lw s5,24(sp)
+    lw s6,28(sp)
+    lw s7,32(sp)
+    lw s8,36(sp)
+    lw s9,40(sp)
+    lw s10,44(sp)
+    lw s11,48(sp)
     
-    
+    addi sp,sp,52
+
     ret
+
 
 m0_dimen_error:
     addi a0,x0,2
