f448766bb43878dd0c690edabd0b1508f71f084f
root <longpeng96@gmail.com>
2022-04-25 08:43:07 +0800

    finish

--- tests/assembly/test_write_matrix.s
+++ tests/assembly/test_write_matrix.s
@@ -1,6 +1,11 @@
 .import ../../src/write_matrix.s
 .import ../../src/utils.s
 
+# the following file_path is different from 
+# official path, since the venus on my machine 
+# can not recognize it.
+# I use the absolute path, please modify path if you
+# want to run this program on you machine
 .data
 m0: .word 1, 2, 3, 4, 5, 6, 7, 8, 9 # MAKE CHANGES HERE TO TEST DIFFERENT MATRICES
 file_path: .asciiz "/root/su20-proj2-starter/tests/outputs/test_write_matrix/student_write_matrix_outputs.bin"
@@ -9,9 +14,9 @@ file_path: .asciiz "/root/su20-proj2-starter/tests/outputs/test_write_matrix/stu
 main:
     # Write the matrix to a file
     la s0,file_path    # s0->filename
-    la s1,m0    # s1->matrix
-    li s2,3     # s2->number of rows
-    li s3,3     # s3->number of cols
+    la s1,m0     # s1->matrix
+    li s2,3      # s2->pointer to number of rows
+    li s3,3      # s3->pointer to number of cols
 
     #a0->filename
     #a1->matrix in memory
@@ -22,6 +27,7 @@ main:
     mv a1,s1
     mv a2,s2
     mv a3,s3
+
     jal write_matrix
 
 
