8b4032389434a883a29f7a0c2f8f36d704aa69b9
root <longpeng96@gmail.com>
2022-04-29 08:28:00 +0800

    finish load 3 matrix

--- src/classify.s
+++ src/classify.s
@@ -21,39 +21,70 @@ classify:
     #   main.s -m -1 <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
 
 
-
-
+    #TODO: Check the number of argument
 
 
 	# =====================================
     # LOAD MATRICES
     # =====================================
 
+    #TODO: Prologue
+    /*
+    sw s0,0(sp)
+    sw s1,4(sp)
+    sw s2,8(sp)
+    sw s3,12(sp)
+    sw s4,16(sp)
+    sw s5,20(sp)
+    sw s6,24(sp)
+    sw s7,28(sp)
+    sw s8,32(sp)
+    sw s9,36(sp)
+    sw s10,40(sp)
+    sw s11,44(sp)
+    */
+    
 
-
-
-
+    lw t0,1(a1)
+    lw t1,2(a1)
+    lw t2,3(a1)
+    lw t3,4(a1)
+    TODO:stack
+    addi sp,sp,
+    sw t0,0(sp)
+    sw t1,16(sp)
+    sw t2,32(sp)
+    sw t3,72(sp)
 
     # Load pretrained m0
-
-
-
-
-
+    lw t0,0(sp)
+    mv a0,t0
+    mv a1,s0
+    mv a2,s1
+    jal read_matrix
+    sw s0,4(sp)
+    sw s1,8(sp)
+    sw a0,12(sp)
 
     # Load pretrained m1
-
-
-
-
-
+    lw t0,16(sp)
+    mv a0,t0
+    mv a1,s0
+    mv a2,s1
+    jal read_matrix
+    sw s0,20(sp)
+    sw s1,24(sp)
+    sw a0,28(sp)
 
     # Load input matrix
-
-
-
-
-
+    lw t0,32(sp)
+    mv a0,t0
+    mv a1,s0
+    mv a2,s1
+    jal read_matrix
+    sw s0,36(sp)
+    sw s1,40(sp)
+    sw a0,44(sp)
 
     # =====================================
     # RUN LAYERS
@@ -61,20 +92,19 @@ classify:
     # 1. LINEAR LAYER:    m0 * input
     # 2. NONLINEAR LAYER: ReLU(m0 * input)
     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    # hidden_layer(s12) =matmul(m0,input)
 
     
+    # calculate the height 
+    # and width of hidden_layer
+
+    # relu(hidden_layer)
+
+    # scores=matmul(m1,hidden_layer)
+
+    # calculate the height and
+    # width of scores
 
 
     # =====================================
@@ -82,26 +112,21 @@ classify:
     # =====================================
     # Write output matrix
 
-
-
-
-
     # =====================================
     # CALCULATE CLASSIFICATION/LABEL
     # =====================================
     # Call argmax
 
-
-
-
     # Print classification
+    mv a1,
+    jal print_int
     
 
-
-
     # Print newline afterwards for clarity
+    li a1,'\n'
+    jal print_char
 
-
-
-
+    # TODO:free
+    
+    # TODO: epilogue
     ret
