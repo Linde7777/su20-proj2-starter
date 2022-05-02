7f64ebb851202f584b978ad579dc6327b751f048
root <longpeng96@gmail.com>
2022-05-02 15:18:24 +0800

    fix some bugs

--- src/classify.s
+++ src/classify.s
@@ -1,5 +1,13 @@
 .globl classify
 
+.data
+p0: .word 0
+p1: .word 0
+p2: .word 0
+p3: .word 0
+p4: .word 0
+p5: .word 0
+
 .text
 classify:
     # =====================================
@@ -29,46 +37,65 @@ classify:
     # =====================================
 
     # Prologue
-    lw t0,1(a1)
-    lw t1,2(a1)
-    lw t2,3(a1)
-    lw t3,4(a1)
-    addi sp,sp,-88
+    #lw t0,1(a1)
+    #lw t1,2(a1)
+    #lw t2,3(a1)
+    #lw t3,4(a1)
+
+    # the follwing 4 lines is
+    # in convience of debug
+    # delete these line when you run
+    mv t0,s0
+    mv t1,s1
+    mv t2,s2
+    mv t3,s3
+
+    addi sp,sp,-92
     sw t0,0(sp)
     sw t1,16(sp)
     sw t2,32(sp)
     sw t3,72(sp)
     sw a0,80(sp)
     sw a2,84(sp)
+    sw ra,88(sp)
 
     # Load pretrained m0
-    lw t0,0(sp)
-    mv a0,t0
+    lw a0,0(sp)
+    la s0,p0
+    la s1,p1
     mv a1,s0
     mv a2,s1
     jal read_matrix
-    sw s0,4(sp)
-    sw s1,8(sp)
+    lw t0,0(s0)
+    lw t1,0(s1)
+    sw t0,4(sp)
+    sw t1,8(sp)
     sw a0,12(sp)
 
     # Load pretrained m1
-    lw t0,16(sp)
-    mv a0,t0
+    lw a0,16(sp)
+    la s0,p2
+    la s1,p3
     mv a1,s0
     mv a2,s1
     jal read_matrix
-    sw s0,20(sp)
-    sw s1,24(sp)
+    lw t0,0(s0)
+    lw t1,0(s1)
+    sw t0,20(sp)
+    sw t1,24(sp)
     sw a0,28(sp)
 
     # Load input matrix
-    lw t0,32(sp)
-    mv a0,t0
+    lw a0,32(sp)
+    la s0,p4
+    la s1,p5
     mv a1,s0
     mv a2,s1
     jal read_matrix
-    sw s0,36(sp)
-    sw s1,40(sp)
+    lw t0,0(s0)
+    lw t1,0(s1)
+    sw t0,36(sp)
+    sw t1,40(sp)
     sw a0,44(sp)
 
     # =====================================
@@ -79,30 +106,29 @@ classify:
     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
     
     # hidden_layer =matmul(m0,input)
-    lw a1,4(sp)
-    lw a1,0(a1)
-    lw a2,8(sp)
-    lw a2,0(a2)
-    
-    lw a3,44(sp)
-    lw a4,36(sp)
-    lw a4,0(a4)
-    lw a5,40(sp)
-    lw a5,0(a5)
+    lw s0,4(sp)
+    lw s1,40(sp)
     
     # calculate the size of hidden_layer
     # then malloc hidden_layer
-    mul t0,a1,a5
+    mul t0,s0,s1
+    li t1,4
+    mul t0,t0,t1
     mv a0,t0
     jal malloc
-    mv s0,a0    # s0->hidden_layer
-    sw s0,48(sp)
-    sw a1,52(sp)
-    sw a5,56(sp)
+    mv s2,a0    # s0->hidden_layer
+    sw s2,48(sp)
+    sw s0,52(sp)
+    sw s1,56(sp)
 
     # call matmul()
+    lw a1,4(sp)
+    lw a5,40(sp)
+    lw a2,8(sp)
+    lw a4,36(sp)
     lw a0,12(sp)
-    mv a6,s0
+    lw a3,44(sp)
+    lw a6,48(sp)
     jal matmul
 
     # relu(hidden_layer)
@@ -115,16 +141,15 @@ classify:
 
     # scores=matmul(m1,hidden_layer)
     lw a1,20(sp)
-    lw a1,0(a1)
     lw a2,24(sp)
-    lw a2,0(a2)
-    lw a3,48(sp)
     lw a4,52(sp)
     lw a5,56(sp)
 
     # calculate the size of score
     # then malloc score
     mul t0,a1,a5
+    li t1,4
+    mul t0,t0,t1
     mv a0,t0
     jal malloc
     mv s0,a0    # s0->score
@@ -134,6 +159,7 @@ classify:
 
     # call matmul()
     lw a0,28(sp)
+    lw a3,48(sp)
     mv a6,s0
     jal matmul
 
@@ -174,7 +200,6 @@ classify:
     jal print_char
 
 end:
-    # free space
     lw a0,12(sp)
     jal free
     lw a0,28(sp)
@@ -189,6 +214,7 @@ end:
     # TODO: epilogue
     lw a0,80(sp)
     lw a2,84(sp)
-    addi sp,sp,88
+    lw ra,88(sp)
+    addi sp,sp,92
 
     ret
